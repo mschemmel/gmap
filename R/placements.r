e0b12fdefd342a6d55e002b66b745bca049b50ad
mschemmel <mschemmel@online.de>
2020-07-08 00:47:14 +0200

    update placements function

--- R/placements.r
+++ R/placements.r
@@ -1,18 +1,18 @@
 # create data frame of requested chromosomes
 placements <- function(df, chromosomes, xsize, ysize, telsize){
   targets_chr <- data.frame(targets = as.character(names(table(df$chr))[chromosomes]),
-                            leftborder = as.numeric(calculate.chromosome.position(xsize, length(chromosomes))),
-                            rightborder = as.numeric(calculate.chromosome.position(xsize, length(chromosomes)) + telsize),
-                            size = as.numeric(tapply(df$cM, df$chr, max)[chromosomes]))
-  targets_chr$height <- 900 * (targets_chr$size / max(targets_chr$size))
+                            leftborder = round(as.numeric(calculate.chromosome.position(xsize, length(chromosomes)), 1)),
+                            rightborder = round(as.numeric(calculate.chromosome.position(xsize, length(chromosomes)) + telsize), 1),
+                            size = round(as.numeric(tapply(df$cM, df$chr, max)[chromosomes])), 1)
+  targets_chr$height <- round(900 * (targets_chr$size / max(targets_chr$size)), 1)
 
   # prepare main table of requested chromosomes
   df <- df[df$chr %in% targets_chr$targets, ]
   df <- merge(df, targets_chr, by.x = "chr", by.y = "targets", full = TRUE)
 
   # calculate marker position
-  df$startabsolute <- ysize - (df$height * (df$cM / df$size))
-  df$endabsolute <- ysize - (df$height * ((df$cM + 0.01) / df$size))
+  df$startabsolute <- round(ysize - (df$height * (df$cM / df$size)), 1)
+  df$endabsolute <- round(ysize - (df$height * ((df$cM + 0.01) / df$size)), 1)
 
   return(df)
 }
