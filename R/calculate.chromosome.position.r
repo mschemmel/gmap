5fa48a8dd5da6b8bde8f432b902e9e83d33e4d33
mschemmel <mschemmel@online.de>
2020-10-28 21:20:54 +0100

    formatting position calculation

--- R/calculate.chromosome.position.r
+++ R/calculate.chromosome.position.r
@@ -1,16 +1,8 @@
 calculate.chromosome.position <- function(pSize = NULL, count = NULL) {
-    # Calculates the chromosome position based on actual number of chromosomes
-    #
-    # Args:
-    #   pSize: plot size (arbitrary specified, internally)
-    #   count: number of chromosomes to plot 
-    # Returns:
-    #   Coordinates to plot given number of chromosomes on plot area
-    # set distance (arbitrary defined)
     distance <- 80
     midpoint <- pSize / 2
     
-    # test if count is odd or even to calculate position
+    # test if count (of requested linkage groups) is odd or even to calculate position
     if ((count %% 2) == 0) {
       toLeft <- count / 2
       toRight <- count / 2
@@ -26,9 +18,11 @@ calculate.chromosome.position <- function(pSize = NULL, count = NULL) {
     
       return(calculatedPosition)
     
-    } else {
+    } 
+	else {
       toLeft <- (count - 1) / 2
       toRight <- (count - 1) / 2
+      
       PositionLeft <- seq(midpoint - (distance * toLeft), midpoint, distance)
       PositionLeft <- PositionLeft[1:length(PositionLeft) - 1]
       PositionRight <- seq(midpoint, midpoint + (distance * toRight), distance)
