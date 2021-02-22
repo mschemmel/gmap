0188ffb2120fddd3aef50c4b59560490fc5987ce
mschemmel <mschemmel@online.de>
2021-02-22 20:57:10 +0100

    formatting of example dataset

--- R/linmap.R
+++ R/linmap.R
@@ -3,8 +3,6 @@
 #' @description Table of linkage groups and markers and their corresponding recombination frequency (cM).
 #' @name linmap
 #' @title Linkage map dataset
-#' 
+#' @format A data frame with 2890 rows and 3 variables
 #' @docType data
-#'
 #' @usage data(linmap)
-NULL
